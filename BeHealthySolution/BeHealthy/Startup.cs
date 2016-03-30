12ab0ce145ab8034c12dad75c799f842aa5a75f7
emam128 <ah.emam92@gmail.com>
2016-03-30 16:59:55 +0200

    v1
    this is the project

---
+++ BeHealthySolution/BeHealthy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BeHealthy.Startup))]
+namespace BeHealthy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
