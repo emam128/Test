dbff37113edd0dff5edce677cb88f5328be4f571
emam128 <ah.emam92@gmail.com>
2016-03-30 17:04:09 +0200

    my p

---
+++ BeHealthy/Controllers/HealthyFoodController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace BeHealthy.Controllers
+{
+    public class HealthyFoodController : Controller
+    {
+        public static List<Food> Foods = new List<Food>()
+        {
+            new Food {Id = 1, Name = "Banana", Code = "Ban-12", CaloriesPerKg = 120},
+            new Food {Id = 2, Name = "Orange", Code = "ora-45", CaloriesPerKg = 56},
+            new Food {Id = 3, Name = "Lemon", Code = "lem-890", CaloriesPerKg = 30}
+        };
+        //
+        // GET: /HealthyFood/
+        public ActionResult Index(string name)
+        {
+            if(string.IsNullOrEmpty(name))
+            return View(Foods);
+            else
+            {
+                return View(Foods.FindAll(c => c.Name.ToLower() == name.ToLower()));
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Foods.RemoveAll(x => x.Id == id);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var fod = Foods.First(c => c.Id == id);
+            return View(fod);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Food food)
+        {
+            Foods.RemoveAll(c => c.Id == food.Id);
+            Foods.Add(food);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Food food)
+        {
+            food.Id = Foods.Max(c => c.Id) + 1;
+            Foods.Add(food);
+            return RedirectToAction("Index");
+        }
+
+
+	}
+
+    public class Food
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Code { get; set; }
+        public Double CaloriesPerKg { get; set; }
+    }
+}
---
+++ BeHealthy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BeHealthy.MvcApplication" Language="C#" %>
